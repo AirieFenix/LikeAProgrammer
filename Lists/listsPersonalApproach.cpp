20f9e09c36e0eae5624ea1c2853cab3a3d73c7fd
Ale Zuvic <airiefenix@Barcelona.local>
2016-02-20 21:28:39 -0300

    Personal approach to lists not working, FUCK IT. I commit to not lose the commiting streak

--- Lists/listsPersonalApproach.cpp
+++ Lists/listsPersonalApproach.cpp
@@ -16,27 +16,64 @@ struct node {
 typedef node *studentCollection;
 
 void addRecord(studentCollection &head, int number, int grade);
+void printList(studentCollection &head);
 
 int main () {
 
 	studentCollection registerHead;
+	//cout << registerHead->studentGrade << registerHead->studentNumber;
+	registerHead -> nextPtr = NULL;
 
-	addRecord(registerHead, 42, 24);
+	addRecord(registerHead, 1, 2);
+	addRecord(registerHead, 2, 3);
+	addRecord(registerHead, 3, 4);
+	//cout << registerHead -> studentNumber << " " << registerHead -> studentGrade;
+	//cout << " " << registerHead -> nextPtr;
 
-	cout << registerHead -> studentNumber;
+	printList(registerHead);
 
 	cout<<"\n";
 	return 0;
 }
 
 void addRecord(studentCollection &head, int number, int grade) {
+		
+	node *student = new node;
 
-		node *student = new node;
+	if (head -> nextPtr == NULL && head -> studentGrade == 0 && head -> studentNumber == 0) {
+		student = head;
+		student -> studentGrade = grade;
+		student -> studentNumber = number;
+	}
+
+	else if (head -> nextPtr == NULL && (head -> studentGrade != 0 || head -> studentNumber != 0)) {
+		head -> nextPtr = student;
 		student -> studentGrade = grade;
 		student -> studentNumber = number;
 		student -> nextPtr = NULL;
-		head = student;
-		cout << student << "\n";
+	}
 
-		//cout << head -> studentNumber;
+	if (head -> nextPtr != NULL) {
+		student = head -> nextPtr;
+
+		while (student -> nextPtr != NULL) {
+			student = student -> nextPtr;
+		}
+		student -> studentGrade = grade;
+		student -> studentNumber = number;
+		student -> nextPtr = NULL;
+	}
+}
+
+void printList(studentCollection &head) {
+	
+	node *tracker = new node;
+	tracker = head;
+
+	do {
+		cout << "\nStudent: " << tracker -> studentNumber << ", grade: " << tracker -> studentGrade;
+		if (tracker -> nextPtr != NULL) {
+			tracker = tracker -> nextPtr;
+		} 
+	} while (tracker -> nextPtr != NULL);
 }
